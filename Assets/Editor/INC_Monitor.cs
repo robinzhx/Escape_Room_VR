4984705a03b7627b71586690bee87f304b357801
robinzhx <zhx068@ucsd.edu>
2018-10-29 06:11:11 -0700

    Updated INC_Monitor System v2

--- Assets/Editor/INC_Monitor.cs
+++ Assets/Editor/INC_Monitor.cs
@@ -9,6 +9,7 @@ public class INC_Monitor : EditorWindow
     int currLevel;
 
     int lapsTime;
+    float sinceLastAction;
 
 
     List<string> sceneName;
@@ -16,17 +17,28 @@ public class INC_Monitor : EditorWindow
 
     string currGazeObj;
     string gazeStringData;
+    string currSurveyResult;
+    string SurveyRecordList;
 
     float gazeX, gazeY;
+    List<Vector2> oldGazePoints;
 
     bool stopConfirm, isSurveyEnable;
 
     LightDir GazeDataProvider;
     GameGaze aGlassDataProvider;
+    SurveyController SurveyDataProvider;
 
     GameObject panel;
 
     Texture eyeTex;
+
+    private Material material;
+    private Color backupColor;
+    private Color currColor;
+    Rect lastRect;
+
+    Vector2 ScrollPos;
 
     // Add menu item named "My Window" to the Window menu
     [MenuItem("Window/INC Monitor")]
@@ -35,9 +47,10 @@ public class INC_Monitor : EditorWindow
     {
         //Show existing window instance. If one doesn't exist, make one.
         EditorWindow.GetWindow(typeof(INC_Monitor));
+
     }
 
-    public void Awake()
+    public void start()
     {
         currLevel = 0;
         
@@ -49,6 +62,8 @@ public class INC_Monitor : EditorWindow
 
         currGazeObj = "";
         gazeStringData = "";
+        currSurveyResult = "";
+        SurveyRecordList = "";
 
         gazeX = 0.0f;
         gazeY = 0.0f;
@@ -59,11 +74,11 @@ public class INC_Monitor : EditorWindow
         isSurveyEnable = false;
 
         eyeTex = new Texture2D(200, 200);
+        
     }
 
     void OnGUI()
     {
-        
 
         GUILayout.Label("Scene List", EditorStyles.boldLabel);
         
@@ -100,7 +115,10 @@ public class INC_Monitor : EditorWindow
         GUILayout.Label("StartTime");
         GUILayout.Label("ObjectName");
         GUILayout.EndHorizontal();
-        GUILayout.TextArea(gazeStringData, GUILayout.Height(200));
+        
+        ScrollPos = EditorGUILayout.BeginScrollView(ScrollPos, GUILayout.Height(200));
+        GUILayout.TextArea(gazeStringData, GUILayout.ExpandHeight(true));
+        EditorGUILayout.EndScrollView();
 
         GUILayout.Label("aGlass Data", EditorStyles.boldLabel);
         GUILayout.BeginHorizontal();
@@ -110,44 +128,118 @@ public class INC_Monitor : EditorWindow
         GUILayout.TextField("" + gazeY);
         GUILayout.EndHorizontal();
 
-        GUILayout.BeginHorizontal();
+
+        /*GUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
         GUILayout.Box(Resources.Load("/Textures/eye") as Texture, GUILayout.Width(200), GUILayout.Height(200));
         GUILayout.Box(Resources.Load("/Textures/eye") as Texture, GUILayout.Width(200), GUILayout.Height(200));
         GUILayout.FlexibleSpace();
-        GUILayout.EndHorizontal();
+        GUILayout.EndHorizontal();*/
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        backupColor = GUI.color;
+        GUI.color = currColor;
+        GUILayout.Box("", GUILayout.Width(400), GUILayout.Height(200));
+        lastRect = GUILayoutUtility.GetLastRect();
+        GUI.color = backupColor;
+        GUILayout.FlexibleSpace();
+        Handles.color = Color.yellow;
+        foreach (Vector2 g in oldGazePoints)
+            Handles.DrawSolidDisc(new Vector3(g.x * 400 + lastRect.center.x - 200, g.y * 200 + lastRect.center.y - 100, 0), new Vector3(0.0f, 0.0f, 1.0f), 3.0f);
+        Handles.color = new Color(1.0f, 1.0f, 1.0f, 0.3f);
+        Handles.DrawSolidDisc(new Vector3(gazeX * 400 + lastRect.center.x - 200, gazeY * 200 + lastRect.center.y - 100, 0), new Vector3(0.0f, 0.0f, 1.0f), 10.0f);
+        Handles.color = Color.red;
+        // Handles.DrawLine(new Vector3(0, 0.0f, 0), new Vector3(20.0f, 20.0f, 0));
+        Handles.DrawSolidDisc(new Vector3(gazeX * 400 + lastRect.center.x - 200 , gazeY * 200 + lastRect.center.y - 100, 0), new Vector3(0.0f, 0.0f, 1.0f), 5.0f);
+        EditorGUILayout.EndHorizontal();
 
         GUILayout.Label("Survey Control", EditorStyles.boldLabel);
         GUILayout.BeginHorizontal();
-        EditorGUILayout.DelayedTextField("Loop Lap Time(s)", lapsTime.ToString());
+        lapsTime = EditorGUILayout.DelayedIntField("Lapse Time(s)", lapsTime);
+        EditorGUILayout.IntField((int)sinceLastAction);
+        
+        GUILayout.Label((isSurveyEnable ? "Status: On" : "Status: Off"));
         if (GUILayout.Button("Post Survey Now"))
         {
-            isSurveyEnable = true;
+            GameObject tmp;
+            if (tmp = GameObject.Find("HeadMenus"))
+            {
+                tmp.GetComponent<SurveyController>().toggle(true);
+                
+            }
+        }
+        if (GUILayout.Button("Turn Off Survey"))
+        {
+            GameObject tmp;
+            if (tmp = GameObject.Find("HeadMenus"))
+            {
+                tmp.GetComponent<SurveyController>().toggle(false);
+            }
         }
         GUILayout.EndHorizontal();
+        ScrollPos = EditorGUILayout.BeginScrollView(ScrollPos, GUILayout.Height(80));
+        GUILayout.TextArea(SurveyRecordList, GUILayout.ExpandHeight(true));
+        EditorGUILayout.EndScrollView();
     }
 
     void Update()
     {
-
+        currColor = Color.grey;
         if (EditorApplication.isPlaying && !EditorApplication.isPaused)
         {
             if (stopConfirm)
             {
                 stopConfirm = false;
                 gazeStringData = "";
+                SurveyRecordList = "";
             }
-            if (isSurveyEnable)
+            /*if (isSurveyEnable)
             {
+                panel = Instantiate(Resources.Load("SurveyPanel") as GameObject) as GameObject;
                 enableSurvey();
+                isSurveyEnable = false;
+            }*/
+
+            GameObject tmp;
+            if (tmp = GameObject.Find("HeadMenus"))
+            {
+                isSurveyEnable = tmp.GetComponent<SurveyController>().getStatus();
             }
-            
+
             GetGazeData();
+
+            if (lapsTime != 0 && !isSurveyEnable)
+            {
+                sinceLastAction += Time.deltaTime;
+
+                if (sinceLastAction > lapsTime)
+                {
+                    if (tmp)
+                    {
+                        tmp.GetComponent<SurveyController>().toggle(true);
+                        isSurveyEnable = tmp.GetComponent<SurveyController>().getStatus();
+                    }
+                    sinceLastAction -= lapsTime;
+                }
+            }
+
+            //oldGazePoints.Add(new Vector2(gazeX, gazeY));
+
+            if (oldGazePoints.Count > 150)
+            {
+                oldGazePoints.Remove(oldGazePoints[0]);
+            }
+
+            if (gazeX == 0 || gazeY == 0)
+                currColor = new Color(0.96f,0.76f,0.05f);
             Repaint();
         }
         else
         {
-            gazeStringData = "Waiting for Editor to Play";
+            gazeStringData = (gazeStringData == "" ? "Waiting for Editor to Play" : gazeStringData);
+            SurveyRecordList = (SurveyRecordList == "" ? "Waiting for Editor to Play" : SurveyRecordList);
+            sinceLastAction = 0;
             if (!stopConfirm)
             {
                 Repaint();
@@ -158,22 +250,120 @@ public class INC_Monitor : EditorWindow
 
     private void enableSurvey()
     {
-        panel = GameObject.Find("SurveyPanel").gameObject;
+        //panel = GameObject.Find("SurveyPanel").gameObject;
         panel.gameObject.SetActive(true);
-        panel.transform.position = GameObject.Find("Head").transform.position + new Vector3(0, 0, 1.0f);
+        panel.transform.parent = GameObject.Find("Head").transform;
     }
 
     void GetGazeData()
     {
         GazeDataProvider = FindObjectOfType<LightDir>();
         aGlassDataProvider = FindObjectOfType<GameGaze>();
-        gazeX = aGlassDataProvider.getCurrX();
-        gazeY = aGlassDataProvider.getCurrX();
-        string newGazeObj = GazeDataProvider.getCurrGazeObjName();
-        if (newGazeObj != currGazeObj)
+        SurveyDataProvider = FindObjectOfType<SurveyController>();
+        if (aGlassDataProvider && GazeDataProvider && SurveyDataProvider)
         {
-            currGazeObj = newGazeObj;
-            gazeStringData = newGazeObj + "\n" + gazeStringData;
+            gazeX = aGlassDataProvider.getCurrX();
+            gazeY = aGlassDataProvider.getCurrY();
+            string newGazeObj = GazeDataProvider.getCurrGazeObjName();
+            string newSurveyResult = SurveyDataProvider.getSurveyResult();
+            if (newGazeObj != currGazeObj)
+            {
+                currGazeObj = newGazeObj;
+                gazeStringData = newGazeObj + "\n" + gazeStringData;
+            }
+            if (newSurveyResult != currSurveyResult)
+            {
+                currSurveyResult = newSurveyResult;
+                gazeStringData = "*******Survey Mark******\n" + gazeStringData;
+                SurveyRecordList = currSurveyResult + "\n" + SurveyRecordList;
+            }
         }
+    }
+
+    void glcreate()
+    {
+        GUILayout.BeginHorizontal(EditorStyles.helpBox);
+
+        // Reserve GUI space with a width from 10 to 10000, and a fixed height of 200, and 
+        // cache it as a rectangle.
+        Rect layoutRectangle = GUILayoutUtility.GetRect(10, 10000, 200, 200);
+
+        if (Event.current.type == EventType.Repaint)
+        {
+            // If we are currently in the Repaint event, begin to draw a clip of the size of 
+            // previously reserved rectangle, and push the current matrix for drawing.
+            GUI.BeginClip(layoutRectangle);
+            GL.PushMatrix();
+
+            // Clear the current render buffer, setting a new background colour, and set our
+            // material for rendering.
+            GL.Clear(true, false, Color.black);
+            if (material = new Material(Shader.Find("Hidden/Internal-Colored")))
+                material.SetPass(0);
+            else
+                Debug.Log("error set pass");
+
+
+            // Start drawing in OpenGL Quads, to draw the background canvas. Set the
+            // colour black as the current OpenGL drawing colour, and draw a quad covering
+            // the dimensions of the layoutRectangle.
+            GL.Begin(GL.QUADS);
+            GL.Color(Color.black);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(layoutRectangle.width, 0, 0);
+            GL.Vertex3(layoutRectangle.width, layoutRectangle.height, 0);
+            GL.Vertex3(0, layoutRectangle.height, 0);
+            GL.End();
+
+            // Start drawing in OpenGL Lines, to draw the lines of the grid.
+            GL.Begin(GL.LINES);
+
+            // Store measurement values to determine the offset, for scrolling animation,
+            // and the line count, for drawing the grid.
+            int offset = (Time.frameCount * 2) % 50;
+            int count = (int)(layoutRectangle.width / 10) + 20;
+
+            for (int i = 0; i < count; i++)
+            {
+                // For every line being drawn in the grid, create a colour placeholder; if the
+                // current index is divisible by 5, we are at a major segment line; set this
+                // colour to a dark grey. If the current index is not divisible by 5, we are
+                // at a minor segment line; set this colour to a lighter grey. Set the derived
+                // colour as the current OpenGL drawing colour.
+                Color lineColour = (i % 5 == 0
+                    ? new Color(0.5f, 0.5f, 0.5f) : new Color(0.2f, 0.2f, 0.2f));
+                GL.Color(lineColour);
+
+                // Derive a new x co-ordinate from the initial index, converting it straight
+                // into line positions, and move it back to adjust for the animation offset.
+                float x = i * 10 - offset;
+
+                if (x >= 0 && x < layoutRectangle.width)
+                {
+                    // If the current derived x position is within the bounds of the
+                    // rectangle, draw another vertical line.
+                    GL.Vertex3(x, 0, 0);
+                    GL.Vertex3(x, layoutRectangle.height, 0);
+                }
+
+                if (i < layoutRectangle.height / 10)
+                {
+                    // Convert the current index value into a y position, and if it is within
+                    // the bounds of the rectangle, draw another horizontal line.
+                    GL.Vertex3(0, i * 10, 0);
+                    GL.Vertex3(layoutRectangle.width, i * 10, 0);
+                }
+            }
+
+            // End lines drawing.
+            GL.End();
+
+            // Pop the current matrix for rendering, and end the drawing clip.
+            GL.PopMatrix();
+            GUI.EndClip();
+        }
+
+        // End our horizontal 
+        GUILayout.EndHorizontal();
     }
 }
