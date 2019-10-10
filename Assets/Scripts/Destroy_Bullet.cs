461cda1465a015d0da75d16f0a4997d68e134636
robinzhx <zhx068@ucsd.edu>
2019-10-10 13:07:06 -0700

    Christian Lay - adding bullets/colt to VST room
    Implementation of the targets and distractors in the VST room.  Target bullets delete upon touching the colt. Deletion script also keeps track of the number of bullets deleted (if a win condition of finding 10 targets was implemented).

---
+++ Assets/Scripts/Destroy_Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_Bullet : MonoBehaviour {
+
+    private int win_count;
+    private int bullets_found;
+
+	// Use this for initialization
+	void Start () {
+        win_count = 3;
+        bullets_found = 0;
+	}
+
+    // Delete this bullet if collides with pistol
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "target_bullet")
+        {
+            Destroy(collision.gameObject);
+            bullets_found++;
+        }
+        if(bullets_found == win_count)
+        {
+            Debug.Log("You win!");
+        }
+    }
+
+}
