200728dc005d7f1207d056aa8f907b47903a5352
robinzhx <zhx068@ucsd.edu>
2019-10-14 12:51:06 -0700

    Finished Layout of Visual Search Task Room
    Any further updates will most likely be minor.
    Put up police tape and shadows upstairs so people can't travel up.
    Implemented a timer with 300 seconds (can edit in the CountDown Timer script).

---
+++ Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour {
+
+    float currTime = 0f;
+    float startTime = 300f;
+    public Text countdownText;
+
+	// Use this for initialization
+	void Start () {
+        currTime = startTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countdownText.color = Color.red;
+        currTime -= 1 * Time.deltaTime;
+        countdownText.text = currTime.ToString("0");
+        if(currTime <= 0)
+        {
+            currTime = 0;
+        }
+	}
+}
