a992d1f40b2aefbaeb5431e9d9e2bc9c8399835f
weichenliu <wel008@ucsd.ede>
2019-10-29 13:59:50 -0700

    Record eyeopenness data in eyetracking stream

--- Assets/Scripts/ProEyeGazeVST.cs
+++ Assets/Scripts/ProEyeGazeVST.cs
@@ -45,15 +45,20 @@ public class ProEyeGazeVST : MonoBehaviour
 
         // 0 - 2d coordinate of left eye
         // 2 - 2d coordinate of right eye
-        // 4 - 3d direction of left eye
+        // 4 - 3d direction of left eye,
         // 7 - 3d direction of right eye
         // 10 - 3d position of combined hit spot
         // 13 - 3d position of head
         // 16 - 3d forward direction of head
         // 19 - 3d velocity of head
         // 22 - 3d angular velocity of head
+        // 25 - left eye openness
+        // 26 - right eye openness
+        // 27 - 3d position of chest IMU**
+        // 30 - 3d forward direction of chest IMU**
+
         liblsl.StreamInfo inf =
-            new liblsl.StreamInfo("ProEyeGaze", "Gaze", 2 + 2 + 3 + 3 + 3 + 3 + 3 + 3 + 3, 50, liblsl.channel_format_t.cf_float32,
+            new liblsl.StreamInfo("ProEyeGaze", "Gaze", 27, 50, liblsl.channel_format_t.cf_float32,
                 "ProEye");
         markerStream = new liblsl.StreamOutlet(inf);
     }
@@ -74,12 +79,37 @@ public class ProEyeGazeVST : MonoBehaviour
         Vector3 GazeOriginCombinedLocal, GazeDirectionCombinedLocal;
         Vector3 GazeOriginLeftLocal, GazeDirectionLeftLocal;
         Vector3 GazeOriginRightLocal, GazeDirectionRightLocal;
-        if (SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal) &&
-            SRanipal_Eye.GetGazeRay(GazeIndex.LEFT, out GazeOriginLeftLocal, out GazeDirectionLeftLocal) &&
-            SRanipal_Eye.GetGazeRay(GazeIndex.RIGHT, out GazeOriginRightLocal, out GazeDirectionRightLocal))
+        float leftOpenness;
+        float rightOpenness;
+
+        SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out GazeOriginCombinedLocal, out GazeDirectionCombinedLocal);
+        if(SRanipal_Eye.GetGazeRay(GazeIndex.LEFT, out GazeOriginLeftLocal, out GazeDirectionLeftLocal))
         {
+            SRanipal_Eye.GetEyeOpenness(EyeIndex.LEFT, out leftOpenness);
         }
-        else return;
+        else
+        {
+            leftOpenness = 0;
+        }
+
+        if(SRanipal_Eye.GetGazeRay(GazeIndex.RIGHT, out GazeOriginRightLocal, out GazeDirectionRightLocal))
+        {
+            SRanipal_Eye.GetEyeOpenness(EyeIndex.RIGHT, out rightOpenness);
+        }
+        else
+        {
+            rightOpenness = 0;
+        }
+
+        if (!SRanipal_Eye.GetPupilPosition(EyeIndex.LEFT, out pupilPos_L))
+        {
+            leftOpenness = 0;
+        }
+        if (!SRanipal_Eye.GetPupilPosition(EyeIndex.RIGHT, out pupilPos_R))
+        {
+            rightOpenness = 0;
+        }
+
 
         Vector3 GazeDirectionCombined = Camera.main.transform.TransformDirection(GazeDirectionCombinedLocal);
         Vector3 camPos = Camera.main.transform.position - Camera.main.transform.up * 0.05f;
@@ -97,9 +127,8 @@ public class ProEyeGazeVST : MonoBehaviour
         Vector3 headsetVelocity = VRTK_SDK_Bridge.GetHeadsetVelocity();
         Vector3 headsetAngVelocity = VRTK_SDK_Bridge.GetHeadsetAngularVelocity();
 
-        if (SRanipal_Eye.GetPupilPosition(EyeIndex.LEFT, out pupilPos_L) &&
-            SRanipal_Eye.GetPupilPosition(EyeIndex.RIGHT, out pupilPos_R))
-        {
+        
+        
             // 0 - 2d coordinate of left eye
             // 2 - 2d coordinate of right eye
             // 4 - 3d direction of left eye
@@ -109,6 +138,10 @@ public class ProEyeGazeVST : MonoBehaviour
             // 16 - 3d forward direction of head
             // 19 - 3d velocity of head
             // 22 - 3d angular velocity of head
+            // 25 - left eye openness
+            // 26 - right eye openness
+            // 27 - 3d position of chest IMU**
+            // 30 - 3d forward direction of chest IMU**
 
             RaycastHit hit;
             Physics.Raycast(lighter.transform.position, lighter.transform.forward, out hit);
@@ -122,7 +155,9 @@ public class ProEyeGazeVST : MonoBehaviour
                               ", 3DHead: " + lighter.transform.position + 
                               ", 3DHeadForward: " + headset.transform.forward + 
                               ", 3DHeadVelocity: " + headsetVelocity +
-                              ", 3DHeadAngVelocity: " + headsetAngVelocity);
+                              ", 3DHeadAngVelocity: " + headsetAngVelocity +
+                              ", LeftEyeOpenness: " + leftOpenness +
+                              ", RightEyeOpenness: " + rightOpenness);
             float[] tempSample =
             {
                 pupilPos_L.x,
@@ -150,13 +185,10 @@ public class ProEyeGazeVST : MonoBehaviour
                 headsetAngVelocity.x,
                 headsetAngVelocity.y,
                 headsetAngVelocity.z,
+                leftOpenness,
+                rightOpenness
             };
             markerStream.push_sample(tempSample);
-        }
-        else
-        {
-            Debug.Log("GetPupilPosition Failed");
-        }
     }
 
     void OnDestroy()
