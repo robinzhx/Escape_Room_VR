5bb99511769d1d6d39c2ad73bcbe5df0c9c97c26
Christian Lay-Geng <claygeng@ucsd.edu>
2019-12-20 16:42:45 -0800

    Shrunk NoPuzzle Room to MobiLab size, target bullets appear in order
    Fixed room constraints to the size of the MobiLab so regardless if people walk past the blue border they do not hit the wall. Also made it so target bullets only appear when the previous one is found (as requested by ChiYuan) so the people have to search more.

---
+++ Assets/Scripts/Spawn_Bullets_In_Order.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Bullets_In_Order : MonoBehaviour {
+
+    public GameObject[] bullets;
+    public int count = 0;
+    
+	// Use this for initialization
+	void Start () {
+        bullets[1].SetActive(false);
+        bullets[2].SetActive(false);
+        bullets[3].SetActive(false);
+        bullets[4].SetActive(false);
+        bullets[5].SetActive(false);
+        bullets[6].SetActive(false);
+        bullets[7].SetActive(false);
+        bullets[8].SetActive(false);
+        bullets[9].SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.tag == "target_bullet")
+        {
+            count++;
+            bullets[count + 1].SetActive(true);
+            Debug.Log("Next target revealed is bullet" + count);
+        }
+	}
+}
