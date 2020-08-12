0e0ac2315c87e074fee19169c443e358a61a1fac
WeichenLiu <WeichenLiu@users.noreply.github.com>
2020-08-12 13:05:19 -0700

    Remove background audio in room 2 and add grab stream to escape rooms

---
+++ Assets/GrabStreamerPuzzle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LSL;
+using NewtonVR;
+using UnityEngine;
+
+public class GrabStreamerPuzzle : MonoBehaviour {
+
+    private liblsl.StreamOutlet markerStream;
+
+	// Use this for initialization
+	void Start ()
+    {
+        liblsl.StreamInfo inf =
+            new liblsl.StreamInfo("GrabMarker", "Markers", 1, 0, liblsl.channel_format_t.cf_string, "controller");
+        markerStream = new liblsl.StreamOutlet(inf);
+	}
+
+    public void SendGrabMarkerLeft(NVRInteractable interactable)
+    {
+        string name = "";
+        //if (interactable.GetComponent<UniqueID>())
+        //    name = interactable.GetComponent<UniqueID>().guid;
+        //else 
+            name = interactable.name;
+        string markerString = "Left hand grabs:" + name;
+        Debug.Log(markerString);
+        string[] tempSample = { markerString };
+        markerStream.push_sample(tempSample);
+    }
+
+    public void SendGrabMarkerRight(NVRInteractable interactable)
+    {
+        string name = "";
+        //if (interactable.GetComponent<UniqueID>())
+        //    name = interactable.GetComponent<UniqueID>().guid;
+        //else 
+            name = interactable.name;
+        string markerString = "Right hand grabs:" + name;
+        Debug.Log(markerString);
+        string[] tempSample = { markerString };
+        markerStream.push_sample(tempSample);
+    }
+
+}
