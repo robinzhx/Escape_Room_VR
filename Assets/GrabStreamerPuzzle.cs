ffd2f4135b04de62bc1d52d055c18da30f47cdc6
WeichenLiu <WeichenLiu@users.noreply.github.com>
2021-02-11 09:31:11 -0800

    Add recording of vrtk's grab events. Fix rotation check codes.

--- Assets/GrabStreamerPuzzle.cs
+++ Assets/GrabStreamerPuzzle.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using LSL;
 using NewtonVR;
 using UnityEngine;
+using VRTK;
 
 public class GrabStreamerPuzzle : MonoBehaviour {
 
     private liblsl.StreamOutlet markerStream;
+    public VRTK_InteractGrab left;
+    public VRTK_InteractGrab right;
 
 	// Use this for initialization
 	void Start ()
@@ -14,7 +17,42 @@ public class GrabStreamerPuzzle : MonoBehaviour {
         liblsl.StreamInfo inf =
             new liblsl.StreamInfo("GrabMarker", "Markers", 1, 0, liblsl.channel_format_t.cf_string, "controller");
         markerStream = new liblsl.StreamOutlet(inf);
+        if (left)
+        {
+            left.ControllerStartGrabInteractableObject += Left_ControllerStartGrabInteractableObject;
+        }
+
+        if (right)
+        {
+            right.ControllerStartGrabInteractableObject += Right_ControllerStartGrabInteractableObject;
+        }
 	}
+
+    private void Right_ControllerStartGrabInteractableObject(object sender, ObjectInteractEventArgs e)
+    {
+        string name = "";
+        //if (interactable.GetComponent<UniqueID>())
+        //    name = interactable.GetComponent<UniqueID>().guid;
+        //else 
+        name = e.target.name;
+        string markerString = "Right hand grabs:" + name;
+        Debug.Log(markerString);
+        string[] tempSample = { markerString };
+        markerStream.push_sample(tempSample);
+    }
+
+    private void Left_ControllerStartGrabInteractableObject(object sender, ObjectInteractEventArgs e)
+    {
+        string name = "";
+        //if (interactable.GetComponent<UniqueID>())
+        //    name = interactable.GetComponent<UniqueID>().guid;
+        //else 
+        name = e.target.name;
+        string markerString = "Left hand grabs:" + name;
+        Debug.Log(markerString);
+        string[] tempSample = { markerString };
+        markerStream.push_sample(tempSample);
+    }
 
     public void SendGrabMarkerLeft(NVRInteractable interactable)
     {
